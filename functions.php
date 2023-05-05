f0c59ebf94c89c21af3d91540a9bbda809288aff
Simonebaca <simobaco99@gmail.com>
2023-05-05 18:02:45 +0200

    fix functions

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
-function generate_password($password_length) {
-  $password = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-[]{};:,.<>?',
-   ceil($password_length/strlen($x)) )),1,$password_length);
-  return $password;
-}
+$password_length = $_GET["password_length"];
+$password = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-[]{};:,.<>?', ceil($password_length/strlen($x)) )),1,$password_length);
+echo $password;
 ?>
