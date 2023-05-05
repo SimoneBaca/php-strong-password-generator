eedc960b0b06bab5ab6591df0fb349dd79c933f0
Simonebaca <simobaco99@gmail.com>
2023-05-05 17:23:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!--Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure. 
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. 
+Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice,
+ spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+</body>
+
+</html>
+
+
+
+
