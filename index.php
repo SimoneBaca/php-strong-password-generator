2a2c897069a45735c4b47d203a129861f9093b00
Simonebaca <simobaco99@gmail.com>
2023-05-05 17:39:35 +0200

    add pass generator

--- index.php
+++ index.php
@@ -27,6 +27,13 @@ Verificato il corretto funzionamento del nostro codice,
 </body>
 
 </html>
+<!-- aggiungo generatore di pass casuale della lunghezza inserita-->
+<?php
+$password_length = $_GET["password_length"];
+$password = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-[]{};:,.<>?', ceil($password_length/strlen($x)) )),1,$password_length);
+echo $password;
+
+
 
 
 
